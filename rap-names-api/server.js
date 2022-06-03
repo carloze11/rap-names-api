e23a52bb98cd19ed5dfbd4c46ce570a9070a9393
Carlos Estrada <cestrada1109@gmail.com>
2022-06-03 14:22:41 -0500

    working rapper api

---
+++ rap-names-api/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+const rappers = {
+    '21 avage': {
+        'birthAge':  29,
+        'birthName': "Sheyaa Bin Abraham-Joseph",
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'birthAge':  29,
+        'birthName': "Chancelor Bennett",
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown': {
+        'birthAge':  0,
+        'birthName': "Unknown",
+        'birthLocation': 'Unknown'
+    }
+}
+
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+   const rapperName = request.params.name.toLowerCase()
+   if (rappers[rapperName] )
+   response.json(rappers)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}! Betta go catch it!`)
+})
